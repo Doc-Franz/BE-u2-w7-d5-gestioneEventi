85400a23329641c48ea9c15e3c96fd10c7465811
Doc-Franz <federico.franzi94@gmail.com>
2025-02-21 17:38:58 +0100

    second commit

--- src/main/java/com/example/gestioneEventi/controller/UserController.java
+++ src/main/java/com/example/gestioneEventi/controller/UserController.java
@@ -1,11 +1,42 @@
 package com.example.gestioneEventi.controller;
 
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.gestioneEventi.exception.EmailDuplicateException;
+import com.example.gestioneEventi.exception.UsernameDuplicateException;
+import com.example.gestioneEventi.payload.request.UserDto;
+import com.example.gestioneEventi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/new")
+    public ResponseEntity<String> insertUser(@RequestBody UserDto userDto, BindingResult validation){
+
+        try {
+            if (validation.hasErrors()) {
+
+                String errorMessage = "Errore nella validazione: \n";
+
+                for (ObjectError error : validation.getAllErrors()) {
+                    errorMessage += error.getDefaultMessage();
+                }
+            }
+
+            String insertUserMessage = userService.insertuser(userDto);
+            return new ResponseEntity<>(insertUserMessage, HttpStatus.OK);
+        } catch (EmailDuplicateException | UsernameDuplicateException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+    }
 }
--- src/main/java/com/example/gestioneEventi/service/UserService.java
+++ src/main/java/com/example/gestioneEventi/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.gestioneEventi.service;
 
+import com.example.gestioneEventi.exception.EmailDuplicateException;
+import com.example.gestioneEventi.exception.UsernameDuplicateException;
 import com.example.gestioneEventi.model.User;
 import com.example.gestioneEventi.payload.request.UserDto;
 import com.example.gestioneEventi.repository.UserRepository;
@@ -15,12 +17,26 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    public String insertuser(User user){
+    public String insertuser(UserDto userDto){
+        checkDuplicatedKey(userDto.getUsername(), userDto.getEmail());
+        User user = dto_entity(userDto);
         userRepository.save(user);
         return "L'utente " + user.getUsername() + " con id " + user.getId() + " è stato salvato correttamente";
     }
 
     // ❗❗ I campi di registrazione dovranno essere convertity nell'entity di utente
+
+    // verificare eventuali campi duplicati --> la gestione try/catch affidata al controller
+    public void checkDuplicatedKey(String username, String email){
+
+        if(userRepository.existByUsername(username)){
+            throw new UsernameDuplicateException("L'username inserito è già stato utilizzato");
+        }
+
+        if(userRepository.existByEmail(email)){
+            throw new EmailDuplicateException("L'email inserita è già stata utilizzata");
+        }
+    }
 
     // travaso DTO -> ENTITY
     public User dto_entity(UserDto userDto){
